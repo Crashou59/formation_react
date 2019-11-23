00b162fe01ffbe92a742accfc9bc4f0be3ff55af
Sebastien Morel <sebastien.morel@davidson.fr>
2019-11-23 12:52:02 +0100

    state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,12 @@
 import React from 'react';
 
 export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { nickname: 'titi' };
+    }
 
     render() {
-        return <span>Hey !! {this.props.nickname} !</span>;
+        return <span>Hey !! {this.state.nickname} !</span>;
     }
 }
