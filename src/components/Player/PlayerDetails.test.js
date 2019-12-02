1047db4f36850d6bcca06f603d3a717bba21f7f3
Norchene <norchene@MacBook-Pro-de-Norchene.local>
2019-12-02 18:45:23 +0100

    add PlayerDetails unit test with snapshot in fail

--- src/components/Player/PlayerDetails.test.js
+++ src/components/Player/PlayerDetails.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import { PlayerDetails } from './PlayerDetails';
 
 describe('PlayerDetails coponent', () => {
@@ -10,6 +11,13 @@ const wrapper = shallow (<PlayerDetails player={firstPlayer} />);
 it('Should render player details component', () => {
 expect(wrapper.find('p').length).toEqual(3);
 expect(wrapper.find(".Player-details").exists()).toEqual(true);
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<PlayerDetails player={firstPlayer} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 });
