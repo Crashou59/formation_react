cfb2831f73bcbce818385d39dd8bfeaf10b237ac
Sebastien Morel <sebastien.morel@davidson.fr>
2019-12-08 16:03:21 +0100

    3 ts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,14 @@ import React from 'react';
 type MyProps = {
     nickname: string;
 };
-export class Header extends React.Component<MyProps> {
+type MyState = {
+    nickname: string;
+};
+export class Header extends React.Component<MyProps, MyState> {
+
+    state: MyState = { nickname: 'titi' };
 
     render() {
-        return <span>Hey !! {this.props.nickname} !</span>
+        return <span>Hey !! {this.state.nickname} !</span>
     }
 }
