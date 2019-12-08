ab793eac8e8596e858ea4b3ae2c209e3cd71c524
Sebastien Morel <sebastien.morel@davidson.fr>
2019-12-08 16:05:43 +0100

    4 ts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,17 @@ export class Header extends React.Component<MyProps, MyState> {
 
     state: MyState = { nickname: 'titi' };
 
+    componentDidMount() {
+        this.updateNickname(this.props);
+    }
+
+    updateNickname(props: MyProps) {
+        this.setState({
+            nickname: props.nickname
+        });
+    }
+
+
     render() {
         return <span>Hey !! {this.state.nickname} !</span>
     }
